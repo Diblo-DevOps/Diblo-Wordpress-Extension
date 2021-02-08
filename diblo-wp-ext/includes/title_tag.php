753d9f592b580cd795fa8e3c7d627125ac6ad2d5
Diblo <Diblo@users.noreply.github.com>
2021-02-08 05:29:54 +0100

    Complete remodel and improvements

--- diblo-wp-ext/includes/title_tag.php
+++ diblo-wp-ext/includes/title_tag.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Add title tag
- * 
+ *
  * Plugin Name: Diblo Wordpress Extension
  * Author: Henrik Ankersø
  * Author URI: https://www.diblo.dk/
- * 
+ *
  * Filters:
  *  diblo_autogen_title_tags: (bool) false
  *
@@ -14,113 +14,169 @@
 // die if accessed directly
 defined('ABSPATH') || die('No direct script access allowed!');
 
+$diblo_attr_pattern = '/%s[ ]*=[ ]*((?<quote>"|\')(?:(?!\k<quote>).)*\k<quote>|[^ ]*)/i';
+
 if ((bool) apply_filters('diblo_autogen_title_tags', false) === true) {
 
-    # https://stackoverflow.com/a/25778430/678611
-    function rel2abs($rel, $base)
+    function build_url($parts)
     {
-        /* return if already absolute URL */
-        if (parse_url($rel, PHP_URL_SCHEME) != '')
-            return ($rel);
+        $url = $parts['scheme'] . '://';
 
-        /* queries and anchors */
-        if ($rel[0] == '#' || $rel[0] == '?')
-            return ($base . $rel);
-
-        /*
-         * parse base URL and convert to local variables:
-         * $scheme, $host, $path
-         */
-        extract(parse_url($base));
-
-        /* remove non-directory element from path */
-        $path = preg_replace('#/[^/]*$#', '', $path);
-
-        /* destroy path if relative url points to root */
-        if ($rel[0] == '/')
-            $path = '';
-
-        /* dirty absolute URL */
-        $abs = '';
-
-        /* do we have a user in our URL? */
-        if (isset($user)) {
-            $abs .= $user;
-
-            /* password too? */
-            if (isset($pass))
-                $abs .= ':' . $pass;
-
-            $abs .= '@';
+        if (! empty($parts['user'])) {
+            $url .= $parts['user'];
         }
 
-        $abs .= $host;
+        if (! empty($parts['pass'])) {
+            $url .= ':' . $parts['pass'];
+        }
 
-        /* did somebody sneak in a port? */
-        if (isset($port))
-            $abs .= ':' . $port;
+        if (! empty($parts['user'])) {
+            $url .= '@';
+        }
 
-        $abs .= $path . '/' . $rel;
+        $url .= $parts['host'];
 
-        /* replace '//' or '/./' or '/foo/../' with '/' */
-        $re = array(
-            '#(/\.?/)#',
-            '#/(?!\.\.)[^/]+/\.\./#'
-        );
-        for ($n = 1; $n > 0; $abs = preg_replace($re, '/', $abs, - 1, $n)) {}
+        if (! empty($parts['port'])) {
+            $url .= ':' . $parts['port'];
+        }
 
-        /* absolute URL is ready! */
-        return ($scheme . '://' . $abs);
+        if (! empty($parts['path'])) {
+            $url .= $parts['path'];
+        }
+
+        if (! empty($parts['query'])) {
+            $url .= '?' . $parts['query'];
+        }
+
+        if (! empty($parts['fragment'])) {
+            $url .= '#' . $parts['fragment'];
+        }
+
+        return $url;
     }
 
-    add_filter('the_content', function ($content) {
+    function absurl($url, $base)
+    {
+        $base = parse_url($base);
+        $_url = parse_url($url);
+
+        if (! empty($_url['scheme'])) {
+            // The URL is already absolute
+            return $url;
+        }
+
+        if (! empty($_url['host'])) {
+            // The URL is only missing the scheme
+            return $base["scheme"] . ':' . $url;
+        }
+
+        if (! empty($_url['path'])) {
+            // Combine path and overwrite base url query and fragment
+            unset($base["query"]);
+            unset($base["fragment"]);
+
+            if (substr($_url['path'], 0, 1) != '/') {
+                $array = explode('/', $_url['path']);
+
+                if (! empty($base['path'])) {
+                    $_array = explode('/', $base['path']);
+
+                    # Remove the file and/or empty path name(s)
+                    $_array = array_slice($_array, 1, - 1);
+
+                    $array = array_merge($_array, $array);
+                }
+
+                $path = array();
+                foreach ($array as $dir) {
+                    if ($dir == '..') {
+                        array_pop($path);
+                    } elseif ($dir != '.') {
+                        $path[] = $dir;
+                    }
+                }
+                $_url['path'] = "/" . implode('/', $path);
+            }
+        } elseif (! empty($_url['query'])) {
+            // Overwrite base url query and fragment
+            unset($base["fragment"]);
+        }
+        // else: Overwrite base url fragment
+
+        return build_url(array_merge($base, $_url));
+    }
+
+    function get_attr_value($att, $string)
+    {
+        global $diblo_attr_pattern;
+
+        preg_match(sprintf($diblo_attr_pattern, $att), $string, $att_value);
+
+        if (count($att_value) <= 0) {
+            return null;
+        }
+
+        if (array_key_exists('quote', $att_value)) {
+            return substr($att_value[1], 1, - 1);
+        }
+
+        return $att_value[1];
+    }
+
+    function handle_title_attributes($content)
+    {
+        global $wp, $diblo_attr_pattern;
+
         if (empty($content)) {
             return $content;
         }
 
-        preg_match_all('/<a ([^>]+)>(.+?)<\/a>/i', $content, $matches, PREG_SET_ORDER);
+        preg_match_all('/<a([^>]+)>(.*?)<\/a>/i', $content, $matches, PREG_SET_ORDER);
 
         foreach ($matches as $matche) {
-            if (strpos(strtolower($matche[1]), ' title') === false) {
-                $title = '';
+            $attributes = $matche[1];
+            $link_text = $matche[2];
 
-                // Use the page title for url if we have a title
-                if (preg_match('/href=("[^"]+"|\'[^\']+\'|[^ ]+)/i', $matche[1], $url_matche) !== false) {
-                    $url = str_replace(array(
-                        '"',
-                        "'"
-                    ), "", $url_matche[1]);
-                    $url = rel2abs($url, home_url($wp->request));
+            // Only handle a non specified or empty title if
+            // the href is specified and not empty
+            $href = get_attr_value('href', $attributes);
+            $title = get_attr_value('title', $attributes);
 
-                    $title = the_title_attribute(array(
-                        'post' => url_to_postid($url)
-                    ));
-                }
+            if (! empty($href) && empty($title)) {
+                // Use the page title for url
+                $url = absurl($href, home_url($wp->request));
+                $args = array(
+                    'echo' => false,
+                    'post' => url_to_postid($url)
+                );
+
+                $title = the_title_attribute($args);
 
                 // Use the link text if we do not have a page title
-                if (! empty($title)) {
-                    $title = $matche[2]; // link text
+                if (empty($title)) {
+                    $title = $link_text;
 
-                    // Use the alt tag text if we have an image link
-                    if (preg_match('/<img[^>]+alt=("[^"]+"|\'[^\']+\'|[^ ]+)/i', $title, $alt_matche) !== false) {
-                        $title = str_replace(array(
-                            '"',
-                            "'"
-                        ), "", $alt_matche[1]); // tag text
+                    // Use the alt tag value if the link contains an image
+                    if (strpos('<img ', $link_text) !== false) {
+                        $title = get_attr_value('alt', $link_text);
                     }
 
                     $title = strip_tags($title); // Remove tags
                 }
 
-                // Adds title if we have a title
+                // Add title attribute if we have a title value
                 if (! empty($title)) {
-                    $replace = "<a " . $matche[1] . " title=\"" . $title . "\">" . $matche[2] . "</a>";
+                    $attributes = preg_replace(sprintf($diblo_attr_pattern, 'title'), '', $attributes); // Removes old title attribute if any
+                    $attributes .= " title=\"" . ucfirst(esc_html($title)) . "\""; // Add title attribute
 
-                    $content = str_replace($matche[0], $replace, $content);
+                    $content = str_replace($matche[0], "<a" . $attributes . ">" . $link_text . "</a>", $content);
                 }
             }
         }
 
         return $content;
-    });
+    }
+
+    add_filter('wp_nav_menu', 'handle_title_attributes', 99999);
+    add_filter('the_content', 'handle_title_attributes', 99999);
 }
